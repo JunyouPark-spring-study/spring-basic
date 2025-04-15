99445d20e4f3f371b34777bf40f4ca7466442f31
Junyou Park <pjy008008@gmail.com>
2025-04-15 14:21:10 +0900

    Spring Core Principles Part 2 – Applying Object-Oriented Principles

--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MemoryMemberRepository implements MemberRepository {
+    //static으로 선언하였기 때문에 다른 참조값을 가지더라도 같은 store공간에 접근한다.
     private static Map<Long, Member> store = new HashMap<>();
     @Override
     public void save(Member member) {

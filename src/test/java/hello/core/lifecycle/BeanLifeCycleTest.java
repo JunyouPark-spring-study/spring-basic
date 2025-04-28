f618b27467f15ee9602e9e4069a6f70ff9cc06ad
Junyou Park <pjy008008@gmail.com>
2025-04-28 13:30:58 +0900

    feat(NetworkClient): how to use init and destroy method by using @Bean annotation

--- src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -18,7 +18,8 @@ public class BeanLifeCycleTest {
 
     @Configuration
     static class LifeCycleConfig {
-        @Bean
+        //destroyMethod는 close, shutdown이라는 함수명을 자동으로 추론함, 사용하기 싫으면 "" 사용
+        @Bean(initMethod = "init", destroyMethod = "close")
         public NetworkClient networkClient() {
             NetworkClient networkClient = new NetworkClient();
             networkClient.setUrl("http://hello-spring.dev");
